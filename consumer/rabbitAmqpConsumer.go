3ecc975475b8314a172a6f55fca954a1d585f8ce
zero <zero@sqrls.de>
2019-03-27 18:07:38 +0100

    started on RabbitMQ AMQP 0.91 producer/consumer

---
+++ consumer/rabbitAmqpConsumer.go
@@ -0,0 +1,85 @@
+package consumer
+
+import (
+	"fmt"
+	"github.com/trivago/gollum/core"
+	"github.com/streadway/amqp"
+	"sync"
+)
+
+type RabbitAmqpConsumer struct {
+	core.SimpleConsumer
+	connectionString string
+	enableDebugLog bool
+	queueName string
+	ampqConsumerId string
+	amqpConnection *amqp.Connection
+	amqpChannel *amqp.Channel
+
+}
+
+func init() {
+	core.TypeRegistry.Register(RabbitAmqpConsumer{})
+}
+
+func (cons *RabbitAmqpConsumer) Configure(conf core.PluginConfigReader) {
+	//get connection string, default to the amqp testing defaults.
+	cons.connectionString = conf.GetString("connectionString", "amqp://guest:guest@localhost:5672/")
+	cons.enableDebugLog = conf.GetBool("debugLog", true)
+	cons.queueName = conf.GetString("queueName","")
+	cons.ampqConsumerId = fmt.Sprintf("%s_%s", "gollum_rabbidAmpq_consumer", conf.GetID())
+}
+
+func(cons *RabbitAmqpConsumer) Consume(workers *sync.WaitGroup) {
+	go cons.startConnection()
+	cons.ControlLoop()
+}
+
+func(cons *RabbitAmqpConsumer) debugLog(msg string) {
+	if cons.enableDebugLog {
+		cons.SimpleConsumer.GetLogger().Debug(msg)
+	}
+}
+
+
+func (cons *RabbitAmqpConsumer) logErrorAndDie(err error, msg string) {
+	if err != nil {
+		cons.SimpleConsumer.GetLogger().Error("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func(cons *RabbitAmqpConsumer) startConnection() {
+	cons.debugLog("starting connection")
+	var err error
+	cons.amqpConnection, err = amqp.Dial(cons.connectionString)
+	cons.logErrorAndDie(err, "Failed to connect to RabbitMQ")
+
+	cons.debugLog("opening channel")
+	cons.amqpChannel, err = cons.amqpConnection.Channel()
+	cons.logErrorAndDie(err, "Failed to open a channel")
+
+
+	cons.amqpChannel.Consume(
+		cons.queueName,
+		cons.ampqConsumerId,
+		true,  //auto-ack
+		false,
+		false,
+		false,
+		nil,
+	)
+}
+
+func(cons *RabbitAmqpConsumer) handleConsume(deliveries <-chan amqp.Delivery, done chan error) {
+	fmt.Println("waiting for rabbitmq deliveries...")
+	for d := range deliveries {
+		d.Ack(false)
+		s := string(d.Body)
+		cons.debugLog("messge="+s)
+		cons.Enqueue(d.Body)
+	}
+}
+
+
+
---
+++ producer/rabbitAmqpProducer.go
@@ -0,0 +1,78 @@
+package producer
+
+import (
+	"fmt"
+	"sync"
+	"github.com/trivago/gollum/core"
+	"github.com/streadway/amqp"
+)
+
+type RabbitAmqpProducer struct {
+	core.BufferedProducer
+	connectionString string
+	enableDebugLog bool
+	exchangeName string
+	queueName string
+	ampqConsumerId string
+	amqpConnection *amqp.Connection
+	amqpChannel *amqp.Channel
+}
+
+func init() {
+	core.TypeRegistry.Register(RabbitAmqpProducer{})
+}
+
+func (prod *RabbitAmqpProducer) Configure(conf core.PluginConfigReader) {
+	//get connection string, default to the amqp testing defaults.
+	prod.connectionString = conf.GetString("connectionString", "amqp://guest:guest@localhost:5672/")
+	prod.enableDebugLog = conf.GetBool("debugLog", true)
+	prod.exchangeName = conf.GetString("exchangeName","")
+	prod.queueName = conf.GetString("queueName","")
+	prod.ampqConsumerId = fmt.Sprintf("%s_%s", "gollum_rabbidAmpq_producer", conf.GetID())
+}
+
+func (prod *RabbitAmqpProducer) pushMessage(msg *core.Message) {
+	prod.debugLog("sending message")
+	amqpMsg := amqp.Publishing {
+		DeliveryMode: amqp.Persistent,
+		Timestamp:    msg.GetCreationTime(),
+		ContentType:  "text/plain",
+		Body:         []byte("Go Go AMQP!"),
+	}
+
+	prod.amqpChannel.Publish(prod.exchangeName,prod.queueName,false,false, amqpMsg)
+}
+
+// Produce writes to stdout or stderr.
+func (prod *RabbitAmqpProducer) Produce(workers *sync.WaitGroup) {
+	defer prod.WorkerDone()
+
+	prod.AddMainWorker(workers)
+	prod.MessageControlLoop(prod.pushMessage)
+}
+
+func(prod *RabbitAmqpProducer) debugLog(msg string) {
+	if prod.enableDebugLog {
+		prod.BufferedProducer.GetLogger().Debug(msg)
+	}
+}
+
+
+func (prod *RabbitAmqpProducer) logErrorAndDie(err error, msg string) {
+	if err != nil {
+		prod.BufferedProducer.GetLogger().Error("%s: %s", msg, err)
+		panic(fmt.Sprintf("%s: %s", msg, err))
+	}
+}
+
+func(prod *RabbitAmqpProducer) startConnection() {
+	prod.debugLog("starting connection")
+	var err error
+	prod.amqpConnection, err = amqp.Dial(prod.connectionString)
+	prod.logErrorAndDie(err, "Failed to connect to RabbitMQ")
+
+	prod.debugLog("opening channel")
+	prod.amqpChannel, err = prod.amqpConnection.Channel()
+	prod.logErrorAndDie(err, "Failed to open a channel")
+
+}
